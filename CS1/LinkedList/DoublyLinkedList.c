ec1bde36293d7cfc7151f02d48b70f09b7b8ff66
Tyler Guerrero <Guerrero.tyler3@gmail.com>
2021-04-13 15:05:25 -0400

    Updated Doubly Linked List

--- CS1/LinkedList/DoublyLinkedList.c
+++ CS1/LinkedList/DoublyLinkedList.c
@@ -120,6 +120,139 @@ void print_list(node *head)
     printf("%d%c", head->data, ((head->next == NULL) ? '\n' : ' '));
 }
 
+node *deleteNth(node *head, int n)
+{
+  node *current, *temp;
+  int i;
+
+  if (head == NULL)
+    return NULL;
+
+  // case for single node
+  if (head->next == NULL && n == 0)
+  {
+    current = head->next;
+    free(head);
+    return current;
+  }
+
+  // base case
+  if (n == 0)
+  {
+    current = head->next;
+    free(head);
+    current->prev = NULL;
+    return current;
+  }
+
+  current = head;
+  i = 0;
+
+  while ((current->next != NULL) && (i < n - 1))
+  {
+    current = current->next;
+    i++;
+  }
+
+  // case for last node
+  if (current->next == NULL)
+  {
+    printf("Moved to far in the list\n");
+    return head;
+  }
+
+  temp = current->next->next;
+  free(current->next);
+  current->next = temp;
+
+  // case for last node
+  if (temp != NULL)
+    temp->prev = current;
+
+  return head;
+}
+
+node *deleteAlt(node *head)
+{
+  node *current, *next;
+
+  // case for single node
+  if (head == NULL || head->next == NULL)
+    return head;
+
+  current = head;
+  next = head->next;
+
+  // case for even or odd length linkedlist
+  while (current != NULL || next != NULL)
+  {
+    current->next = next->next;
+
+    // case for even length list last node
+    if (next->next == NULL)
+    {
+      free(next);
+      break;
+    }
+
+    next->next->prev = current;
+    free(next);
+    current = current->next;
+
+    // case for odd length list last node
+    if (current != NULL)
+      next = current->next;
+  }
+
+  return head;
+}
+
+node *insert_sorted(node *head, int n)
+{
+  node *current, *sorted_node;
+
+  // base case
+  if (n <= head->data)
+  {
+    sorted_node = create_node(n);
+    sorted_node->next = head;
+    head->prev = sorted_node;
+    return sorted_node;
+  }
+
+  // case for single node
+  if ((head->next == NULL) && (n <= head->data))
+  {
+    sorted_node = create_node(n);
+    sorted_node->next = head;
+    head->prev = sorted_node;
+    return sorted_node;
+  }
+
+  current = head;
+  while ((current->next != NULL) && (n > current->next->data))
+    current = current->next;
+
+  // case for last node
+  if (current->next == NULL)
+  {
+    sorted_node = create_node(n);
+    current->next = sorted_node;
+    sorted_node->prev = current;
+    return head;
+  }
+
+  sorted_node = create_node(n);
+
+  sorted_node->prev = current;
+  sorted_node->next = current->next;
+
+  current->next->prev = sorted_node;
+  current->next = sorted_node;
+
+  return head;
+}
+
 int *create_array(int n)
 {
   int i;
@@ -150,7 +283,7 @@ void print_array(int *array, int n)
 
 int main(void)
 {
-  int *array, i, len = 10;
+  int *array, i, len = 9;
   node *head = NULL;
   srand(time(NULL));
 
@@ -170,4 +303,30 @@ int main(void)
 
   head = tail_delete(head);
   print_list(head);
+
+  head = deleteAlt(head);
+  print_list(head);
+
+  head = deleteNth(head, 3);
+  print_list(head);
+
+  head = deleteNth(head, 6);
+  print_list(head);
+
+  head = tail_delete(head);
+  print_list(head);
+
+  head = tail_insert(head, 44);
+  print_list(head);
+
+  head = tail_delete(head);
+  print_list(head);
+
+  head = tail_insert(head, 44);
+  print_list(head);
+
+  head = destroy_list(head);
+  free(array);
+
+  return 0;
 }
