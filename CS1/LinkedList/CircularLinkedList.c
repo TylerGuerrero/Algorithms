601c2da9d4c2520e6fd83cbee39cfab6eca130da
Tyler Guerrero <Guerrero.tyler3@gmail.com>
2021-04-15 12:59:28 -0400

    Updated Circular List

--- CS1/LinkedList/CircularLinkedList.c
+++ CS1/LinkedList/CircularLinkedList.c
@@ -182,9 +182,126 @@ void clever_print_circular_list_alternate(node *head)
   }
 }
 
+void deleteNth(node **head, node **tail, int n)
+{
+  node *current, *temp;
+  int i;
+
+  if (*head == NULL || *tail == NULL)
+    return;
+  // case for single node
+  else if ((*head)->next == *head && n == 0)
+  {
+    free(*head);
+    *head = *tail = NULL;
+    return;
+  }
+  else if (n == 0)
+  {
+
+  }
+  else
+  {
+    current = *head;
+    i = 0;
+
+    while ((current->next != *head) && (i < n - 1))
+    {
+      current = current->next;
+      i++;
+    }
+
+    // case for last node
+    if (current->next == *head)
+    {
+      printf("Went to far in the list\n");
+      return;
+    }
+
+    // case for last node
+    if (current->next->next == *head)
+    {
+      free(current->next);
+      *tail = current;
+      (*tail)->next = *head;
+      return;
+    }
+
+    temp = current->next->next;
+    free(current->next);
+    current->next = temp;
+  }
+}
+
+void deleteAlt(node **head)
+{
+  node *current, *next;
+
+  // case for single node
+  if (*head == NULL || (*head)->next == *head)
+    return;
+
+  current = *head;
+  next = (*head)->next;
+
+  while (1)
+  {
+    current->next = next->next;
+    free(next);
+    current = current->next;
+
+    // case for last node
+    if (current != *head)
+      next = current->next;
+
+    // case for even or odd length linked list
+    if (current == *head || next == *head)
+      break;
+  }
+}
+
+void ourobors_insert_sorted(node **head, node **tail, int n)
+{
+  node *current, *sorted_node;
+
+  if (*head == NULL || *tail == NULL)
+  {
+    *head = *tail = create_node(n);
+    (*head)->next = *head;
+    return;
+  }
+  // case for first node
+  else if (n <= (*head)->data)
+  {
+    sorted_node = create_node(n);
+    sorted_node->next = *head;
+    *head = sorted_node;
+    (*tail)->next = *head;
+    return;
+  }
+  else
+  {
+    current = *head;
+    while (current->next != *head && n > current->next->data)
+      current = current->next;
+
+    // case for last node
+    if (current->next == *head)
+    {
+      current->next = create_node(n);
+      *tail = current->next;
+      (*tail)->next = *head;
+    }
+
+    sorted_node = create_node(n);
+    sorted_node->next = current->next;
+    current->next = sorted_node;
+  }
+}
+
 int main(void)
 {
-  int array[] = {15, 24, 7, 1, 32}, n = 5, i;
+  int array[] = {15, 24, 7, 1, 32, 13, 56, 8, 41, 10}, n = 10, i;
 	node *head = NULL, *tail = NULL;
 
 	for (i = 0; i < n; i++)
@@ -199,6 +316,21 @@ int main(void)
   ouroboros_head_delete_fancy(&head, &tail);
   print_circular_list(head);
   ouroboros_tail_delete_fancy(&head, &tail);
+  print_circular_list(head);
+
+  deleteNth(&head, &tail, 3);
+  print_circular_list(head);
+  ouroboros_head_insert_fancy(&head, &tail, 3);
+  print_circular_list(head);
+  deleteNth(&head, &tail, 8);
+  print_circular_list(head);
+
+  ouroboros_tail_insert_fancy(&head, &tail, 13);
+  ouroboros_tail_insert_fancy(&head, &tail, 12);
+  //  ouroboros_tail_insert_fancy(&head, &tail, 88);
+  print_circular_list(head);
+
+  deleteAlt(&head);
   print_circular_list(head);
 
   head = free_circular_list(head);
